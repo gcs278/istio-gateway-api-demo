1d5954d7dccb582f207dd93263b3620ff4c7b5af
Andrew McDermott <aim@frobware.com>
2022-07-29 13:56:56 +0100

    install: fix some quoting issues

--- helper-scripts/install-istio-gwapi.sh
+++ helper-scripts/install-istio-gwapi.sh
@@ -3,7 +3,7 @@
 set -eu
 
 thisdir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"
-pushd $thisdir > /dev/null 2>&1
+pushd "$thisdir" > /dev/null 2>&1
 
 DOWNLOAD_DIR="/tmp/istio-download"
 mkdir -p "$DOWNLOAD_DIR"
@@ -15,7 +15,7 @@ oc create namespace istio-system --dry-run=client -o yaml | oc apply -f -
 oc adm policy add-scc-to-group anyuid system:serviceaccounts:istio-system
 oc adm policy add-scc-to-user privileged -n istio-system -z istio-ingressgateway-service-account
 
-: ${ISTIO_RELEASE:="https://github.com/istio/istio/releases/download/1.14.2/istio-1.14.2-linux-amd64.tar.gz"}
+: "${ISTIO_RELEASE:=https://github.com/istio/istio/releases/download/1.14.2/istio-1.14.2-linux-amd64.tar.gz}"
 
 if [[ ! -x "$DOWNLOAD_DIR/bin/istioctl" ]]; then
     echo "Downloading and extracting $ISTIO_RELEASE"
@@ -35,7 +35,7 @@ if [[ "$ISTIO_HOST_NETWORKING" == "true" ]]; then
 fi
 
 # Install Istio for openshift
-istioctl install -y --set profile=openshift --set meshConfig.accessLogFile=/dev/stdout $hostNetArg
+istioctl install -y --set profile=openshift --set meshConfig.accessLogFile=/dev/stdout ${hostNetArg:-}
 
 # Expose openshift route for istio
 # Not really needed, since we make our own DNS records to circumvent openshift-router
